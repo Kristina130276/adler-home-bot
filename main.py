3d50c83f831536d9426d79884c7b2a0a32397872
Kristina130276 <kgegenava1@gmail.com>
2025-06-05 20:01:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from keep_alive import keep_alive
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [[InlineKeyboardButton("📞 Позвонить Юле", url="tel:+79616784444")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    text = (
+        "🏡 Добро пожаловать!\n\n"
+        "📍Предлагаем уютный дом в Адлере, всего в 10 минутах от пляжа.\n"
+        "🛏️ 4 комнаты, 109 м²\n"
+        "🌿 Закрытая территория, центральные коммуникации"
+    )
+    await update.message.reply_text(text, reply_markup=reply_markup)
+
+if __name__ == '__main__':
+    keep_alive()
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
