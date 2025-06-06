6679f6ac0f4c1555f1976763e52775b0b190443c
Kristina130276 <kgegenava1@gmail.com>
2025-06-06 15:05:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("📞 Контакт: +79616784444", callback_data="contact_info")],
+        [InlineKeyboardButton("📞 Написать в WhatsApp", url="https://wa.me/79616784444"),
         [InlineKeyboardButton("📎 Подробнее на Avito", url="https://www.avito.ru/sochi/doma_dachi_kottedzhi/4-k._dom_109_m_4865915984")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
